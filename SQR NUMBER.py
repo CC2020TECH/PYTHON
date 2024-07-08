76653c587763167298180a9416224f5517a559a0
John Doe <johndoe@email.com>
2024-07-08 09:47:23 -0700

    Version 001

---
+++ SQR NUMBER.py
@@ -0,0 +1,3 @@
+import math
+num1 = int(input("N1: "))
+print ("Raiz es:",math.sqrt(num1))
---
+++ SUM NUMBERS.py
@@ -0,0 +1,4 @@
+num1 = int(input("N1: "))
+num2 = int(input("N2: "))
+total=(num1+num2)
+print ("Suma es:",total)
